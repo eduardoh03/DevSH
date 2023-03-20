9f386911b912a9fc58e4cead70637891b3de5691
Eduardo <eduhenriquesrs@gmail.com>
2023-03-20 11:04:41 -0300

    Project created.

---
+++ src/main/java/com/example/devsh/DevShApplication.java
@@ -0,0 +1,13 @@
+package com.example.devsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevShApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevShApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devsh/DevShApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevShApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
