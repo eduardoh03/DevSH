e784bcb11d031efc609651e11ecd770d9c6cd9c3
taohansens <taohansens@gmail.com>
2023-04-15 18:19:54 -0300

    Clean unused files.

--- src/main/java/com/devsh/devsh/controller/UserController.java
+++ src/main/java/com/devsh/devsh/controller/UserController.java
@@ -1,23 +1,21 @@
 package com.devsh.devsh.controller;
 
+import com.devsh.devsh.dto.TokenJWTDTO;
 import com.devsh.devsh.dto.UserDTO;
 import com.devsh.devsh.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/login")
 public class UserController {
     @Autowired
     private UserService service;
     @PostMapping
-    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto) {
-        dto = service.insert(dto);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(dto.getId()).toUri();
-        return ResponseEntity.created(uri).body(dto);
+    public ResponseEntity<TokenJWTDTO> doLogin(@RequestBody @Valid UserDTO userDTO) {
+        TokenJWTDTO token = service.doLogin(userDTO);
+        return ResponseEntity.ok().body(token);
     }
 }
